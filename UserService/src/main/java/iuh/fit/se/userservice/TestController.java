d7efc79eb9bd325b066812490e321dfa95b95fad
GGDuck <vunguyenminhduc1410@gmail.com>
2025-03-02 11:00:19 +0700

    base config complete

---
+++ UserService/src/main/java/iuh/fit/se/userservice/TestController.java
@@ -0,0 +1,15 @@
+package iuh.fit.se.userservice;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/user")
+public class TestController {
+
+    @GetMapping("/test")
+    public String test() {
+        return "Hello World from User Service!";
+    }
+}
