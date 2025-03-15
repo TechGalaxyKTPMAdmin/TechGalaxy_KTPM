b29a2f36fdae685218f5fb07744423fd3f0d6034
quangkid2212 <21071241.quang@student.iuh.edu.vn>
2025-03-15 11:19:02 +0700

    ProductFeedbackController

---
+++ ProductService/src/main/java/iuh/fit/se/productservice/client/CustomerClient.java
@@ -0,0 +1,15 @@
+package iuh.fit.se.productservice.client;
+
+
+import iuh.fit.se.productservice.dto.response.CustomerResponseV2;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+import iuh.fit.se.productservice.dto.response.DataResponse;
+
+@FeignClient(name = "UserService", path = "/customers")
+public interface CustomerClient {
+    @GetMapping("/{id}")
+    DataResponse<CustomerResponseV2> getCustomerById(@PathVariable("id") String id);
+}
---
+++ ProductService/src/main/java/iuh/fit/se/productservice/entities/enumeration/CustomerStatus.java
@@ -0,0 +1,7 @@
+package iuh.fit.se.productservice.entities.enumeration;
+
+public enum CustomerStatus {
+    ACTIVE,
+    INACTIVE,
+    SUSPENDED
+}
